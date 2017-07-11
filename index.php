68208699c547a23c10e746db0f6920ba23eaade3
Radu Onisor <radu.onisor@webventure.ro>
2017-07-11 14:44:42 +0300

    text json

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 	if (stripos($_SERVER['REQUEST_URI'], 'api') !== false) {
 		
 		include 'calcul_zile.php';
-		header("Content-Type: aplication/json");
+		header("Content-Type: text/json");
 
 		if ( is_numeric(substr($_SERVER['REQUEST_URI'], -4)) ) {
 			die(returnLegalDays( substr($_SERVER['REQUEST_URI'], -4), 1 ));
 		}
-		
+
 		die(json_encode(['error' => 'Link-ul trebuie sa contina anul dorit de forma [yyyy]']));
 	}
 
