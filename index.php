52d29a3df55dedeabfae1fdd18e74c1944a0d3c9
Radu Onisor <radu.onisor@webventure.ro>
2017-07-19 10:32:24 +0300

    favicon

--- index.php
+++ index.php
@@ -30,6 +30,8 @@
 	<meta charset="UTF-8" >
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+	<link rel="icon" href="img/favicon.png">
+
 	<!-- Font -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 
